d3145c3878d35f8614458a426627b4c9239c52ed
rootnure <nur.diu.2791@gmail.com>
2024-09-19 00:28:57 +0600

    Different ways to use export in ES6

--- export.js
+++ export.js
@@ -6,5 +6,14 @@ const greetingsMsg = name => {
 // named export
 export const greetDeveloper = name => `Hello developer ${name}. Welcome to ES6.`;
 
+// named export in different way
+const greetNewDeveloper = name => `Hello ${name}. Welcome to next level development`;
+const greetOldDeveloper = name => `Hello developer ${name}. Welcome back to next level development`;
+
+export {
+    greetNewDeveloper,
+    greetOldDeveloper
+}
+
 // default export
 export default greetingsMsg;
--- import.js
+++ import.js
@@ -1,6 +1,8 @@
 // default exported resource can be import using any name
 // named exported resource can only be imported using the same name and need to use destructuring
-import generateMsg, { greetDeveloper } from "./export.js";
+import generateMsg, { greetDeveloper, greetNewDeveloper, greetOldDeveloper } from "./export.js";
 
 console.log(generateMsg('Sabab'));
 console.log(greetDeveloper('Sohel'));
+console.log(greetNewDeveloper('Rajib'));
+console.log(greetOldDeveloper('Mahbub'));
